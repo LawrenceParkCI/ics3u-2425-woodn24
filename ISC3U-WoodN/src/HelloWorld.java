eba031eff3cad7ee31fd988475d72ad7472790d8
342789906 <342789906@C-MXL2393F1J.tdsb.on.ca>
2024-09-20 11:11:16 -0400

    Adding ICS3U project with the hello world code along with more lines.

---
+++ ISC3U-WoodN/src/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * <b> Description:<br><br> </b> This program prints the text hello world <br>
+ * Additionally I have experimented with more print commands <br>
+ * <b><br> Date:<br> </b>2024-09-19 </b> 
+ * @author NathanWood
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Have an increadible day");
+		System.out.println("Have a great code");
+		System.out.println();
+		System.out.println("have a good day");
+		
+
+	}
+}
+
+/*
+ * This is a block comment 
+ * On a block comment you can add multiple lines of code
+ */
