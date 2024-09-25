e0858402828ef0e0592022c87386da256f4b65ba
342789906 <342789906@C-MXL2393F1J.tdsb.on.ca>
2024-09-25 10:38:51 -0400

    I made a timetable for my week at Lawrence Park.

---
+++ ISC3U-WoodN/src/AboutMe.java
@@ -0,0 +1,23 @@
+/**
+ * <b> Description:<br><br> </b> This program prints info about me * Additionally I have experimented with more print commands <br>
+ * <b><br> Date:<br> </b>2024-09-30 </b> 
+ * @author NathanWood
+ */
+public class AboutMe {
+
+	public static void main(String[] args) {
+		// My intro stuff		
+		System.out.println("About me");
+		System.out.println();
+		System.out.println("My name is Nathan");
+		System.out.println("I am in grade 11");
+		System.out.print("My techcher is Ms.Kemp");
+		System.out.println(" and I got to LPCI");
+		System.out.printf(" \"Go Panthers\" ");
+		System.out.format("Date: %-10sTime: %8s","june", "1:30");
+
+	}
+
+}
+
+
--- ISC3U-WoodN/src/HelloWorld.java
+++ ISC3U-WoodN/src/HelloWorld.java
@@ -1,23 +1,16 @@
-/**
- * <b> Description:<br><br> </b> This program prints the text hello world <br>
- * Additionally I have experimented with more print commands <br>
- * <b><br> Date:<br> </b>2024-09-19 </b> 
- * @author NathanWood
- */
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Have an increadible day");
-		System.out.println("Have a great code");
-		System.out.println();
-		System.out.println("have a good day");
+		System.out.printf("\t time\t  |Late Start Time|\tMon\t  |\tTue\t  |\tWed\t  |\tThu\t  |\tFri\t  |\n");
+		System.out.print("__________________|_______________|_______________|_______________|_______________|_______________|_______________|");
+		System.out.printf("\n        9:00-10:15|%15s|%-15s|%-15s|%-15s|%-15s|%-15s|","9:55-10:55 ","   Functions" , "   Comp Sci","    Functions","     Comp Sci","   Functions","   Comp Sci");
+		System.out.printf("\n       10:20-11:40|%15s|%-15s|%-15s|%-15s|%-15s|%-15s|","11:00-12:05","  Comp Sci","    Functions","   Comp Sci","    Functions","    Comp Sci","   Functions");
+		System.out.printf("\n       11:45-12:30|%15s|%-15s|%-15s|%-15s|%-15s|%-15s|","12:10-12:00","     Lunch","     Lunch","     Lunch","     Lunch","     Lunch","     Lunch");
+		System.out.printf("\n        12:40-1:55|%15s|%-15s|%-15s|%-15s|%-15s|%-15s|","1:00-2:00","    Physics" ,"   Marketing", "    Physics","   Marketing","    Physics","   Marketing");
+		System.out.printf("\n         2:00-3:15|%15s|%-15s|%-15s|%-15s|%-15s|%-15s|" ,"2:05-3:15","  Marketing", "    Physics","   Marketing","    Physics","   Marketing","   Physics");
+	
 		
+
 
 	}
 }
-
-/*
- * This is a block comment 
- * On a block comment you can add multiple lines of code
- */
