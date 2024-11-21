40514325678287c915dcb77d87d9c83087c7aa81
342789906 <342789906@C-MXL2393F1J.tdsb.on.ca>
2024-11-21 10:02:54 -0500

    Code for testing spesific parts of larger code

---
+++ ISC3U-WoodN/src/tests/TestCode.java
@@ -0,0 +1,36 @@
+package tests;
+
+import java.util.Scanner;
+
+
+public class TestCode {
+	/**
+	 * Date: Mon, Oct 21, 2024<br>
+	 * Description: A program which asks the user for price and quantity of two items at a grocery store and prints the receipt.
+	 * @author Erik Porteu
+	 * @param args 
+	 */
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Which of the following statments is correct to display Welcome to Java on the console?()Choose all that apply.");
+		// choose all that apply
+		System.out.println("For This section choose all that apply, type correct answers in alphbetical order");
+		// Question 5
+		System.out.println("Which of the following statments is correct to display Welcome to Java on the console?");
+		System.out.println("A) System.out.println('Welcome to java'");
+		System.out.println("B)System.out.println(\"Welcome to Java\");");
+		System.out.println("C)System.out.print(\"Welcome to Java\");");
+		System.out.println("D) System.println('Welcome to Java');");
+		System.out.println("E)System.out.print('Welcome to Java');");
+//		String q5answer = in.nextLine();
+//		char q5 = q5answer.charAt(0);
+		
+
+				
+
+		
+		in.close();
+		 	}
+
+}
